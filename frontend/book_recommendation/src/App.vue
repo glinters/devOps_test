10dff88e61e22aa378dc45c0186a091584ba2a8d
Gatis Linters <gatis.linters@latvenergo.lv>
2023-06-06 13:11:49 +0300

    feat: added vue3 project as frontend

---
+++ frontend/book_recommendation/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import LandingPage from './components/LandingPage.vue'
+import {
+  darkTheme,
+  NConfigProvider,
+  NMessageProvider,
+  NNotificationProvider,
+  NDialogProvider,
+  NGlobalStyle
+} from "naive-ui";
+</script>
+
+<template>
+  <n-config-provider :theme="darkTheme">
+    <n-global-style />
+    <n-message-provider>
+    <n-notification-provider>
+      <n-dialog-provider>
+        <LandingPage/>
+      </n-dialog-provider>
+    </n-notification-provider>
+  </n-message-provider>
+    
+  </n-config-provider>
+  
+</template>
